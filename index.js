3548b02e47715cc2fb93b2118dcf2390bca214d4
Robbin Habermehl <robbin@habermehl.net>
2015-02-12 23:59:24 +0100

    Support Windows file paths
    Closes #5

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var concat = require('gulp-concat');
 var es = require('event-stream');
 var gutil = require('gulp-util');
+var path = require('path');
 
 function cacheTranslations(options) {
   return es.map(function(file, callback) {
     file.contents = new Buffer(gutil.template('$translateProvider.translations("<%= language %>", <%= contents %>);\n', {
       contents: file.contents,
       file: file,
-      language: options.language || file.path.split('/').pop().match(/^(?:[\w]{3,}-)?([a-z]{2}[_|-]?(?:[A-Z]{2})?)\.json$/i).pop()
+      language: options.language || file.path.split(path.sep).pop().match(/^(?:[\w]{3,}-)?([a-z]{2}[_|-]?(?:[A-Z]{2})?)\.json$/i).pop()
     }));
     callback(null, file);
   });
